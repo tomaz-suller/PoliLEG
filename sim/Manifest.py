3281197150d85e09c01e57f14e977293379280d7
tomaz-suller <tomaz.suller@usp.br>
2021-06-28 22:50:12 -0300

    Initial commit

---
+++ sim/Manifest.py
@@ -0,0 +1,12 @@
+action = "simulation"
+sim_tool = "ghdl"
+top_module = "toplevel_tb"
+sim_post_cmd = (
+    "ghdl -r " + top_module 
+    # + " --wave=wave.ghw\n\t\t"
+    # "gtkwave wave.vcd config.gtkw"
+)
+
+modules = {
+    "local" : [ "../testbenches"],
+}
